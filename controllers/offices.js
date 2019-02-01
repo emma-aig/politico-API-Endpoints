8ad658ba58fc1ba2975b88c083e8bd50262f785e
Emmanuel Aigbovbiosa <emma.aig@gmail.com>
2019-02-01 09:01:15 +0100

    Work on get all political offices

--- controllers/offices.js
+++ controllers/offices.js
@@ -1,6 +1,6 @@
 import office from "../database/database";
 
-// Get all political offices
+// Get a specific political office
 export function getOffice (req, res) {
     const id = parseInt(req.params.id, 10);
     office.map((office) => {
@@ -8,12 +8,21 @@ export function getOffice (req, res) {
             return res.status(200).send({
                 success: "true",
                 message: "Data retrieved successfully",
-                office,
+                office: office,
             });
         } 
     });
     return res.status(404).send({
         success: "false",
         message: "Data does not exist",
+    });
+}
+
+// Get all political officies
+export function getOffices (req, res) {
+    res.status(200).send({
+        success: "true",
+        message: "Data retrieved successfully",
+        office: office,
     });
 }
--- router/router.js
+++ router/router.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const offices = require("../controllers/offices");
 
 router.get("/offices/:id", offices.getOffice);
+router.get("/offices/", offices.getOffices);
 
 export default router;
