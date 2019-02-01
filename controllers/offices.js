6f7350e26bcade75d1c1a1404cb1507646ce27ac
Emmanuel Aigbovbiosa <emma.aig@gmail.com>
2019-02-01 10:46:33 +0100

    Work on create political office

--- controllers/offices.js
+++ controllers/offices.js
@@ -26,3 +26,29 @@ export function getOffices (req, res) {
         office: office,
     });
 }
+
+// Create a political office
+export function createOffice (req, res) {
+    if(!req.body.type) {
+        return res.status(400).send({
+            success: "false",
+            message: "type is required",
+        });
+    } else if(!req.body.name) {
+        return res.status(400).send({
+            success: "false",
+            message: "name is required",
+        });
+    }
+    const officeData = {
+        id: office.length + 1,
+        type: req.body.type,
+        name: req.body.name
+    };
+    office.push(officeData);
+    return res.status(201).send({
+        success: "true",
+        message: "Office created successfully",
+        office,
+    });
+}
--- router/router.js
+++ router/router.js
@@ -5,5 +5,6 @@ const offices = require("../controllers/offices");
 
 router.get("/offices/:id", offices.getOffice);
 router.get("/offices/", offices.getOffices);
+router.post("/offices/", offices.createOffice);
 
 export default router;
